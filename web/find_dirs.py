38215292f641c90e1ee13bc8d82d36561c87e6a7
James Chambers <jameschambers2@gmail.com>
2016-04-24 01:26:44 -0400

    Add option to ignore certs to dir finder

--- web/find_dirs.py
+++ web/find_dirs.py
@@ -9,14 +9,21 @@ def main():
                         help="Root HTTP path to scan")
     parser.add_argument('wordlist', type=str,
                         help="Newline separated list of directories to try")
+    parser.add_argument('--no-verify', action='store_true',
+                        help="Don't verify SSL certificates")
     args = parser.parse_args()
+
+    if args.no_verify is not None:
+        verify = False
+    else:
+        verify = True
 
     root_dir = args.root.rstrip('/')
 
     with open(args.wordlist, 'r') as wordlist:
         for line in wordlist:
             cur_dir = line.rstrip('\r\n')
-            r = requests.get(root_dir + '/' + cur_dir)
+            r = requests.get(root_dir + '/' + cur_dir, verify=verify)
             if r.status_code != 404:
                 print '%d: %s' % (r.status_code, cur_dir)
 
