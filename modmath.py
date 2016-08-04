f3b22d6ef45323d13a62bc1e4e44f03431897696
James Chambers <jameschambers2@gmail.com>
2016-08-03 23:47:24 -0400

    modmath style/readability

--- modmath.py
+++ modmath.py
@@ -4,19 +4,22 @@ def mod_fac(n, mod):
         raise Exception("mod_fac: must give non-negative n")
     elif n == 0:
         return 1
-    
+
     result = n % mod
-    for i in range((n % mod)-1, 0, -1):
+    for i in range((n % mod) - 1, 0, -1):
         result = mod_mult(result, i, mod)
     return result
+
 
 def mod_mult(x, y, mod):
     """Modular multiplication"""
     return (x % mod * y % mod) % mod
 
+
 def mod_div(numerator, denominator, mod):
     """Modular division"""
-    return mod_mult(numerator, pow(denominator,mod-2,mod), mod)
+    return mod_mult(numerator, pow(denominator, mod - 2, mod), mod)
+
 
 def mod_choose(n, k, mod):
     """Modular binomial coefficient"""
@@ -25,10 +28,14 @@ def mod_choose(n, k, mod):
     elif k == n:
         return 1
     else:
-        return mod_div(mod_fac(n, mod), mod_mult(mod_fac(k, mod), mod_fac(n-k, mod), mod), mod)
+        # n! / (k! * (n-k)!)
+        return mod_div(mod_fac(n, mod),
+                       mod_mult(mod_fac(k, mod), mod_fac(n-k, mod), mod), mod)
+
 
 def mod_exp(x, y, mod):
-    """Modular exponentiation (can upgrade to use repeated squaring)"""
+    """Modular exponentiation"""
+    # can upgrade to use repeated squaring
     result = 1
     for i in range(0, y):
         result *= (x % mod)
